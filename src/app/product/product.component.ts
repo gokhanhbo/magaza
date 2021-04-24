dbf0b7673bab7c37aa651f0fb6b4ca068586f3a9
gokhanhbo <gokhanhbo@gmail.com>
2021-04-24 20:46:49 +0300

    product names

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -14,11 +14,11 @@ export class ProductComponent implements OnInit {
   products : Product[] = [
     {id:1, name: "laptop", price:2500, categoryId:1, description: "Asus Zenbook", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
     {id:2, name: "Mouse", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
-    {id:2, name: "Mouse", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
-    {id:2, name: "Mouse", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
-    {id:2, name: "Mouse", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
-    {id:2, name: "Mouse", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
-    {id:2, name: "Mouse", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"}
+    {id:2, name: "Book", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
+    {id:2, name: "Music Disk", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
+    {id:2, name: "USB Cable", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
+    {id:2, name: "Tablet", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
+    {id:2, name: "Keyboard", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"}
 
   ]
 
