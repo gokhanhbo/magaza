2edd41ed2f3ac1e85034b4fc1c65f33adb5c1d78
gokhanhbo <gokhanhbo@gmail.com>
2021-04-24 20:23:06 +0300

    filter text

--- src/app/product/product-filter.pipe.ts
+++ src/app/product/product-filter.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from './product';
 
 @Pipe({
   name: 'productFilter'
 })
 export class ProductFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: Product[], filterText?: string): Product[] {
+    filterText = filterText?filterText.toLocaleLowerCase():null
+
+    return filterText?value.filter((p:Product)=>p.name
+    .toLocaleLowerCase().indexOf(filterText)!==-1):value;
   }
 
 }
