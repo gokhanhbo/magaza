454525cf922dea8c4cc947ef96f28910ec598c2e
gokhanhbo <gokhanhbo@gmail.com>
2021-04-24 20:13:34 +0300

    pipe filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
+import { ProductFilterPipe } from './product/product-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CategoryComponent,
-    ProductComponent
+    ProductComponent,
+    ProductFilterPipe
   ],
   imports: [
     BrowserModule,
