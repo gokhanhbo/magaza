e4198685482e87d90a9afc80e6d481ffe667b3db
gokhanhbo <gokhanhbo@gmail.com>
2021-04-24 20:26:16 +0300

    filter

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,7 +10,7 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title = "Ürün Listesi"
-  filterText = "abc"
+  filterText = "m"
   products : Product[] = [
     {id:1, name: "laptop", price:2500, categoryId:1, description: "Asus Zenbook", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
     {id:2, name: "Mouse", price:100, categoryId:2, description: "Logitech", imageUrl:"https://productimages.hepsiburada.net/s/41/375/10688626163762.jpg"},
