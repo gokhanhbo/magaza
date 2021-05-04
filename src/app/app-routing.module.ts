674ab9f66f486a1bbc235e9e1b16bdac7f7a9800
gokhanhbo <gokhanhbo@gmail.com>
2021-05-04 23:22:18 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Product } from './product/product';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products', component: ProductComponent},
+  {path:'', redirectTo: 'products', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
