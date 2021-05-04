a750bc875e6dc49b6d85468917176d605b601693
gokhanhbo <gokhanhbo@gmail.com>
2021-05-04 21:53:56 +0300

    category service & component

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
 import { Category } from './category';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers: [CategoryService]
 })
+
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
-  title="Kategori Listesi"
-  categories : Category[] = [
-    {id:1, name:"Elektronik"},
-    {id:2, name:"Cep Telefonu"},
-    {id:3, name:"Müzik"},
-    {id:4, name:"Kitap"},
-    {id:5, name:"Süpermarket"}
-  ]
-
-  ngOnInit(): void {
+  constructor(
+    private categoryService: CategoryService
+    ) {}
+  title = "Kategori Listesi";
+  categories: Category[];
+  ngOnInit() {
+    this.categoryService.getCategories().subscribe(data=>{
+      this.categories = data
+    });
   }
 
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Category } from '../category/category';
+import { Observable, throwError } from 'rxjs';
+import {tap,catchError} from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  dbpath = 'http://159.89.31.194:4000/categories';
+
+  getCategories():Observable<Category[]>{
+    return this.http
+      .get<Category[]>(this.dbpath).pipe(
+        tap(data=>console.log(JSON.stringify(data))),
+        catchError(this.handleError),
+      );
+  }
+
+  handleError(err: HttpErrorResponse) {
+    let errorMessage =''
+    if(err.error instanceof ErrorEvent){
+      errorMessage = 'Bir hata oluştu '+err.error.message
+    }
+    else {
+      errorMessage = 'Sistemsel bir hata'
+    }
+    return throwError(errorMessage);
+  }
 }
