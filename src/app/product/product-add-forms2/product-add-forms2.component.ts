4d74dd27dd2a8e063b8a13dbc94176365eb3f632
gokhanhbo <gokhanhbo@gmail.com>
2021-05-10 23:31:08 +0300

    .

--- src/app/product/product-add-forms2/product-add-forms2.component.ts
+++ src/app/product/product-add-forms2/product-add-forms2.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Category } from 'src/app/category/category';
+import { AlertifyService } from 'src/app/services/alertify.service';
 import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product';
 
 @Component({
   selector: 'app-product-add-forms2',
   templateUrl: './product-add-forms2.component.html',
   styleUrls: ['./product-add-forms2.component.css'],
-  providers: [CategoryService],
+  providers: [CategoryService, ProductService],
 })
 export class ProductAddForms2Component implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
     private categoryService: CategoryService,
+    private productService: ProductService,
+    private alertifyService: AlertifyService
   ) { }
 
   productAddForm: FormGroup;
@@ -42,6 +46,9 @@ export class ProductAddForms2Component implements OnInit {
     if (this.productAddForm.valid) {
       this.product = Object.assign({}, this.productAddForm.value)
     }
+    this.productService.addProduct(this.product).subscribe(data=>{
+      this.alertifyService.success(data.name + " başarı ile eklendi.")
+    });
   }
 
 }
