3411e3ef25f9055bd1fc1e8c2e3b1cdab0a6ba87
gokhanhbo <gokhanhbo@gmail.com>
2021-05-10 21:06:38 +0300

    http post

--- src/app/product/product-add-forms1/product-add-forms1.component.ts
+++ src/app/product/product-add-forms1/product-add-forms1.component.ts
@@ -2,18 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Category } from 'src/app/category/category';
 import { CategoryComponent } from 'src/app/category/category.component';
+import { AlertifyService } from 'src/app/services/alertify.service';
 import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product';
 
 @Component({
   selector: 'app-product-add-forms1',
   templateUrl: './product-add-forms1.component.html',
   styleUrls: ['./product-add-forms1.component.css'],
-  providers: [CategoryService]
+  providers: [CategoryService, ProductService]
 })
 export class ProductAddForms1Component implements OnInit {
 
-  constructor(private categoryService:CategoryService) { }
+  constructor(
+    private categoryService:CategoryService, 
+    private productService:ProductService,
+    private alertifyService:AlertifyService
+    ) { }
     model : Product = new Product();
     categories: Category[];
 
@@ -24,7 +30,10 @@ export class ProductAddForms1Component implements OnInit {
     }
 
     add(form:NgForm){
-      console.log(form.value.name);
+      this.productService.addProduct(this.model).subscribe(data=>{
+        this.alertifyService.success(data.name + " başarı ile eklendi.")
+      });
+    
 
       
     }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Product } from '../product/product';
 import { Observable, throwError } from 'rxjs';
-import {tap,catchError} from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -10,23 +10,38 @@ export class ProductService {
   constructor(private http: HttpClient) { }
   dbpath = 'http://159.89.31.194:4000/products';
 
-  getProducts(categoryId):Observable<Product[]>{
-let newPath = this.dbpath;
-if(categoryId){
-  newPath =newPath + "?categoryId="+categoryId
-}
+
+  getProducts(categoryId): Observable<Product[]> {
+    let newPath = this.dbpath;
+    if (categoryId) {
+      newPath = newPath + "?categoryId=" + categoryId
+    }
 
     return this.http
       .get<Product[]>(newPath).pipe(
-        tap(data=>console.log(JSON.stringify(data))),
+        tap(data => console.log(JSON.stringify(data))),
         catchError(this.handleError),
       );
   }
 
+  addProduct(product: Product): Observable<Product> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Token'
+      })
+    }
+
+    return this.http.post<Product>(this.dbpath, product, httpOptions).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
   handleError(err: HttpErrorResponse) {
-    let errorMessage =''
-    if(err.error instanceof ErrorEvent){
-      errorMessage = 'Bir hata oluştu '+err.error.message
+    let errorMessage = ''
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'Bir hata oluştu ' + err.error.message
     }
     else {
       errorMessage = 'Sistemsel bir hata'
