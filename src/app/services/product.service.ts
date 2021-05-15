83e2f68c35247b020fc6af08706b454bd51e017d
gokhanhbo <gokhanhbo@gmail.com>
2021-05-15 12:23:20 +0300

    addBasket

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { Product } from '../product/product';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
+import { Basket } from '../sepet/sepet.component';
 
 @Injectable()
 export class ProductService {
@@ -37,6 +38,26 @@ export class ProductService {
       catchError(this.handleError)
     );
   }
+
+
+
+
+  addBasket(basket: Basket): Observable<Product> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Token'
+      })
+    }
+
+    return this.http.post<Basket>(this.dbpath, basket, httpOptions).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  
+  
 
   handleError(err: HttpErrorResponse) {
     let errorMessage = ''
