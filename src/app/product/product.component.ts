bf65ed2ae4670a849c7f013a21d5a15fa8bc3222
gokhanhbo <gokhanhbo@gmail.com>
2021-05-19 00:01:35 +0300

    sepet denemeler

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,13 +3,14 @@ import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from '../services/alertify.service';
 import { ProductService } from '../services/product.service';
 import { Product } from './product';
+import { SepetComponent } from '../sepet/sepet.component';
 
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
-  providers: [ProductService],
+  providers: [ProductService, SepetComponent],
 })
 
 export class ProductComponent implements OnInit {
