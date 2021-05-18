b78673cd14236e9755773f979ac4b083cda4880e
gokhanhbo <gokhanhbo@gmail.com>
2021-05-19 00:02:53 +0300

    sepet

--- src/app/sepet/sepet.component.ts
+++ src/app/sepet/sepet.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Category } from 'src/app/category/category';
-import { AlertifyService } from 'src/app/services/alertify.service';
-import { CategoryService } from 'src/app/services/category.service';
-import { ProductService } from 'src/app/services/product.service';
-import { Product } from '../product/product';
 
 @Component({
   selector: 'app-sepet',
@@ -16,34 +10,6 @@ export class SepetComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-}
-
-
-export class ProductAddForms1Component implements OnInit {
-
-  constructor(
-    private categoryService: CategoryService,
-    private productService: ProductService,
-    private alertifyService: AlertifyService
-  ) { }
-  model: Product = new Product();
-  categories: Category[];
-
-  ngOnInit() {
-    this.categoryService.getCategories().subscribe(data => {
-      this.categories = data
-    });
-  }
-
-  add(form: NgForm) {
-    this.productService.addProduct(this.model).subscribe(data => {
-      this.alertifyService.success(data.name + " başarı ile eklendi.")
-    });
-
-
-
   }
 
 }
