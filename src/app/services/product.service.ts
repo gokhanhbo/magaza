2c394f9c6541bd703f003c594a8388a19f5cdffb
gokhanhbo <gokhanhbo@gmail.com>
2021-05-30 10:32:32 +0300

    .

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -26,6 +26,20 @@ export class ProductService {
       );
   }
 
+  addProduct(product: Product): Observable<Product> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Token'
+      })
+    }
+
+    return this.http
+      .post<Product>(this.sepetpath, product, httpOptions).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
 
   addToCart(product: Product): Observable<Product> {
     const httpOptions = {
