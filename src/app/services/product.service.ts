d8ac0730451512726e9d46fa7daaba976b7dbcb6
gokhanhbo <gokhanhbo@gmail.com>
2021-05-30 10:24:55 +0300

    addtoCart silindi

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -41,29 +41,6 @@ export class ProductService {
     );
   }
 
-  addToCart(product: Product): Observable<Product> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': 'Token'
-      })
-    }
-    
-    return this.http.post<Product>(
-      this.dbpath, product, httpOptions)
-      .pipe(tap(data => console.log(JSON.stringify(data))),
-      catchError(this.handleError)
-    );
-  }
-
-  
-
-
-
-
-  
-
-
   handleError(err: HttpErrorResponse) {
     let errorMessage = ''
     if (err.error instanceof ErrorEvent) {
